e3d6d2d36e1ebb184b1afe5a2a04d88d61aaf3eb
Bryondr <44525204+Bryondr@users.noreply.github.com>
2019-05-24 00:18:49 +0300

    Taze Taze sıcak sıcak gel al kardeş burası DONE

--- FollowSystem.java
+++ FollowSystem.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class FollowSystem
 {
     //constuctor
@@ -9,18 +11,23 @@ public class FollowSystem
 
     public void Follow(UserClass user1 /*follower*/,UserClass user2 /*followed*/)
     {
-        user1.getFollowedList().add(user2);
-        //EĞER OLMAZSA FOR DÖNGÜSÜ İLE YENİ BİR ARRAY LİST TANIMLAYIP İKİSİNİ BİRBİRİNE EŞİTLEYECEKSİN
+        ArrayList<UserClass> list;
+        list = user1.getFollowedList();
+        list.add(user2);
+        user1.setFollowedList(list);
 
     }
 
     public void Unfollow(UserClass user1,UserClass user2)
     {
-        user1.getFollowedList().remove(user2);
+        ArrayList<UserClass> list;
+        list = user1.getFollowedList();
+        list.remove(user2);
+        user1.setFollowedList(list);
     }
 
 
-    public boolean CheckFollow(UserClass user1,UserClass user2)
+    public boolean IsFollow(UserClass user1,UserClass user2)
     {
         for (int i = 0; i < user1.getFollowedList().size(); i++)
         {
@@ -36,11 +43,11 @@ public class FollowSystem
     {
         for (int i =0; i< user1.getFollowedList().size();i++)
         {
-            if (user1.getFollowedList().get(i).equals(user2.getFollowedList().get(i)) )
+            if (user1.getFollowedList().get(i).equals(user2) )
             {
                 for (int j=0; j < user2.getFollowedList().size() ;j++)
                 {
-                    if (user2.getFollowedList().get(j).equals(user1.getFollowedList().get(j)) )
+                    if (user2.getFollowedList().get(j).equals(user1) )
                     {
                         return true;//if both side is following each other return True
                     }
