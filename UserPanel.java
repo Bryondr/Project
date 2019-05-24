97e933cff8fa58ccc8ba08307b098b2eb9806310
Bryondr <44525204+Bryondr@users.noreply.github.com>
2019-05-24 19:05:33 +0300

    Main eklendi tam olmadı tabisi sendeki Paneli koymak lazım

--- UserPanel.java
+++ UserPanel.java
@@ -1,7 +1,15 @@
 public class UserPanel
 {
-    public void SelectionsWindow(UserClass user)
+    public static void main(String args[])
     {
+        UserClass user1 = new UserClass("Mehmet amca","mehmetamca@hotmail.com",1,1,1000);
+        Administrator Admin = new Administrator();
+        Register Registerer = new Register();
+
+
+        UserClass user = Registerer.Registering();
+        Admin.CallBirthday(user);
+
         System.out.println("Welcome " + user.getName() + ", please choose an operation;");
         System.out.println("1. Show Inbox");
         System.out.println("2. Show Outbox");
@@ -10,7 +18,10 @@ public class UserPanel
         System.out.println("5. Show timeline");
         System.out.println("6. Show contact list");
         System.out.println("7. Show Notifications");
+        //user.ShowNotifications(); Bu böyle kullanılmayacak sadece
+        // 7.seçenek seçilirse bu method çalışacak ona göre implement edilmeli!!!!
+        // Switch Case bu işe uygun!!!
         System.out.println("8. Follow People");
-
+        
     }
 }
