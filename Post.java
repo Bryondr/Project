31d065c1da4dba22b85e4b1a86723a64018059c4
Bryondr <44525204+Bryondr@users.noreply.github.com>
2019-05-24 18:38:34 +0300

    Son Hal (Birkaç method tartışılacak)

--- Post.java
+++ Post.java
@@ -1,62 +1,82 @@
-package project;
-
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Post extends UserClass {
+public class Post {
 
     Scanner scanner = new Scanner(System.in);
-    
-    
+
     //Fields
+
     private String content;
-    private String userName;
+    private ArrayList<String> comments;
     private int likes;
     private int shares;
-    private int comments;
-    
+    private int commentCount;
 
 
-    public Post(String content, String userName, int likes, int shares, int comments){
+    public Post(String content, int likes, int shares, int commentCount, ArrayList<String> comments){
         this.content = content;
-        this.userName = userName;
         this.likes = likes;
         this.shares = shares;
+        this.commentCount = commentCount;
         this.comments = comments;
-        
     }
-    
-    public Post (){
-        
-    }
+    public Post (){   }
 
     //Getters and Setters
-    
-    public String getUserName() { return name; }
-    
+
     public String getContent() { return content; }
-    public void setcontent ( String content ) { this.content = content; }
-    
+    public void setContent ( String content ) { this.content = content; }
+
     public int getLikes() {return likes; }
-    public void setLikes() { this.likes = likes; }
-    
+    public void setLikes(int likes) { this.likes = likes; }
+
     public int getShares() { return shares; }
-    public void setShares() { this.shares = shares; }
-    
-    public int getComments() { return comments; }
-    public void setComments() { this.comments = comments; }
+    public void setShares(int shares) { this.shares = shares; }
+
+    public int getCommentCount() { return commentCount; }
+    public void setCommentCount(int commentCount) { this.commentCount = commentCount; }
+
+    public ArrayList<String> getComments() { return comments; }
+    public void setComments(ArrayList<String> comments) { this.comments = comments;}
+
 
     //Some Methods
 
-    public void like (){
+    public void Like (){
         likes = likes+1;
     }
 
-    public void sharePost() {
-        
-        System.out.println("Write what you want to share: ");
-        content = scanner.nextLine();
-        Post post = new Post(content, getUserName(), 0, 0, 0);
-      }
-    
- }
+    public void sharePost(UserClass user)
+    {
 
+        System.out.println("Write what you want to share: ");
+        String cont = scanner.nextLine();
+        System.out.println();
+        setContent(cont);
+
+        ArrayList<Post> List =user.getPostList();
+        List.add(this);
+
+
+        System.out.println("\n\tPosted.");
+        System.out.println("1 to return User Panel - 2 to share another post.");
+
+        int choose = scanner.nextInt();
+        scanner.nextLine();//dummy
+        }
+
+
+    public void Comment(UserClass user)
+    {
+        ArrayList<String> list = new ArrayList<>();
+        list = getComments();
+        System.out.println("Write your comment here: ");
+        String comm = scanner.nextLine();
+        list.add(user.getName()+" : "+comm);
+        setComments(list);
+        int a = getCommentCount()+1;
+        setCommentCount(a);
+    }
+
+}
