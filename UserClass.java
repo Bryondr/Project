5c38a3e7a1f94e7632a310ce3e7c0188317aad25
Bryondr <44525204+Bryondr@users.noreply.github.com>
2019-05-24 18:37:48 +0300

    Son Hal

--- UserClass.java
+++ UserClass.java
@@ -5,25 +5,33 @@ public class UserClass
     //Fields
     private String name;
     private String email;
-    private int dayOfBirth;
+    private int dayOfBirth = 0;
     private int monthOfBirth;
     private int yearOfBirth;
     private ArrayList<String> notificationBox;
-    private ArrayList postList;
-    private ArrayList timeLine;
-    private ArrayList favPosts;
+    private ArrayList<Post> postList;
+    private ArrayList<Post> timeLine;
+    private ArrayList<Post> favPosts;
     private ArrayList<UserClass> followedList;
     private String place;
-    private ArrayList incoming;
-    private ArrayList outgoing;
+    private ArrayList<Message> incoming;
+    private ArrayList<Message> outgoing;
 
 
     public UserClass ()
     {
 
     }
+    public UserClass(String name,String email,int dayOfBirth, int monthOfBirth,int yearOfBirth)
+    {
+        this.name = name;
+        this.email = email;
+        this.dayOfBirth = dayOfBirth;
+        this.monthOfBirth = monthOfBirth;
+        this.yearOfBirth = yearOfBirth;
+    }
 
-    public UserClass(String name, String email, int dayOfBirth,int monthOfBirth,int yearOfBirth, ArrayList<String> notificationBox, ArrayList postList, ArrayList timeLine, ArrayList favPosts, ArrayList<UserClass> followedList, String place, ArrayList incoming, ArrayList outgoing)
+    public UserClass(String name, String email, int dayOfBirth,int monthOfBirth,int yearOfBirth, ArrayList<String> notificationBox, ArrayList<Post> postList, ArrayList<Post> timeLine, ArrayList<Post> favPosts, ArrayList<UserClass> followedList, String place, ArrayList<Message> incoming, ArrayList<Message> outgoing)
     {
         this.name = name;
         this.email = email;
@@ -50,14 +58,14 @@ public class UserClass
     public ArrayList<String> getNotificationBox() { return notificationBox; }
     public void setNotificationBox(ArrayList notificationBox) { this.notificationBox = notificationBox; }
 
-    public ArrayList getPostList() { return postList; }
-    public void setPostList(ArrayList postList) { this.postList = postList; }
+    public ArrayList<Post> getPostList() { return postList; }
+    public void setPostList(ArrayList<Post> postList) { this.postList = postList; }
 
-    public ArrayList getTimeLine() { return timeLine; }
-    public void setTimeLine(ArrayList timeLine) { this.timeLine = timeLine; }
+    public ArrayList<Post> getTimeLine() { return timeLine; }
+    public void setTimeLine(ArrayList<Post> timeLine) { this.timeLine = timeLine; }
 
     public ArrayList getFavPosts() { return favPosts; }
-    public void setFavPosts(ArrayList favPosts) { this.favPosts = favPosts; }
+    public void setFavPosts(ArrayList<Post> favPosts) { this.favPosts = favPosts; }
 
     public ArrayList<UserClass> getFollowedList() { return followedList; }
     public void setFollowedList(ArrayList<UserClass> followedList) { this.followedList = followedList; }
@@ -66,10 +74,10 @@ public class UserClass
     public void setPlace(String place) { this.place = place; }
 
     public ArrayList getIncoming() { return incoming; }
-    public void setIncoming(ArrayList incoming) { this.incoming = incoming; }
+    public void setIncoming(ArrayList<Message> incoming) { this.incoming = incoming; }
 
     public ArrayList getOutgoing() { return outgoing; }
-    public void setOutgoing(ArrayList outgoing) { this.outgoing = outgoing; }
+    public void setOutgoing(ArrayList<Message> outgoing) { this.outgoing = outgoing; }
 
     public int getDayOfBirth() {return dayOfBirth; }
     public void setDayOfBirth(int dayOfBirth) {this.dayOfBirth = dayOfBirth; }
@@ -95,4 +103,5 @@ public class UserClass
                 System.out.println(notificationBox.get(i));
             }
     }
+
 }
