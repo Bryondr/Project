ddf457bdd19e90e70f21dd7d14a39935867adf75
Bryondr <44525204+Bryondr@users.noreply.github.com>
2019-05-26 16:30:44 +0300

    Update Timeline.java

--- Timeline.java
+++ Timeline.java
@@ -1,4 +1,26 @@
-package project;
+public class TimeLine {
 
-public class Timeline extends Post {
+
+    //Gets all posts from timeline
+    public void getTimeLine( UserClass user ){
+
+        for(int i=0; i<user.getPostList().size(); i++)
+        {
+            System.out.println("__________________");
+            System.out.println( i+1 + ". Post\n" + user.getTimeLine().get(i).getSharer()+"\n");
+            System.out.println(user.getTimeLine().get(i).getContent());
+            System.out.println(user.getTimeLine().get(i).getLikes() + " Likes ");
+            System.out.println(user.getTimeLine().get(i).getComments().size()+" Comments");
+            System.out.println("__________________");
+        }
+    }
+    //Gets post from timeline
+    public void getTimeLine( UserClass user, int i )
+    {
+        System.out.println("__________________");
+        System.out.println( i + ". Post\n" + user.getTimeLine().get(i-1).getSharer()+"\n");
+        System.out.println(user.getTimeLine().get(i-1).getContent());
+        System.out.println(user.getTimeLine().get(i-1).getLikes() + "Likes ");
+        System.out.println("__________________");
+    }
 }
