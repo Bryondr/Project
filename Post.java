e7257e0baaa5bf84a4ff2bf3450a79780fc3507e
Bryondr <44525204+Bryondr@users.noreply.github.com>
2019-05-26 16:29:36 +0300

    Like bitmedi

--- Post.java
+++ Post.java
@@ -10,18 +10,22 @@ public class Post {
     private String content;
     private ArrayList<String> comments;
     private int likes;
-    private int shares;
     private int commentCount;
+    private String sharer;
 
 
-    public Post(String content, int likes, int shares, int commentCount, ArrayList<String> comments){
+    public Post(String sharer,String content, int likes, int commentCount, ArrayList<String> comments)
+    {
+        this.sharer = sharer;
         this.content = content;
         this.likes = likes;
-        this.shares = shares;
         this.commentCount = commentCount;
         this.comments = comments;
     }
-    public Post (){   }
+    public Post ()
+    {
+        this.comments = new ArrayList<String>();
+    }
 
     //Getters and Setters
 
@@ -31,52 +35,65 @@ public class Post {
     public int getLikes() {return likes; }
     public void setLikes(int likes) { this.likes = likes; }
 
-    public int getShares() { return shares; }
-    public void setShares(int shares) { this.shares = shares; }
-
     public int getCommentCount() { return commentCount; }
     public void setCommentCount(int commentCount) { this.commentCount = commentCount; }
 
     public ArrayList<String> getComments() { return comments; }
     public void setComments(ArrayList<String> comments) { this.comments = comments;}
 
-
-    //Some Methods
+    public String getSharer() { return sharer; }
+    public void setSharer(String sharer) { this.sharer = sharer;}
+    //Other Methods
 
     public void Like (){
         likes = likes+1;
     }
 
-    public void sharePost(UserClass user)
+    public void sharePost(UserClass user,FollowSystem Following)
     {
-
+        this.sharer = user.getName();
         System.out.println("Write what you want to share: ");
         String cont = scanner.nextLine();
-        System.out.println();
         setContent(cont);
+        ArrayList<Post> list = new ArrayList<>();
 
-        ArrayList<Post> List =user.getPostList();
-        List.add(this);
+        //adds post to user's time line
+        list = user.getTimeLine();
+        list.add(this);
+        user.setTimeLine(list);
+
+        //adds post to user's post list(for main page)
+        list = user.getPostList();
+        list.add(this);
+        user.setPostList(list);
 
 
+        //sends post to followers postList
+        for (int i=0 ;i<user.getUserlist().size();i++)
+        {
+            if (Following.IsFollow(user.getUserlist().get(i),user ))
+            {
+                list = user.getUserlist().get(i).getPostList();
+                list.add(this);
+                user.getUserlist().get(i).setPostList(list);
+                //Control Post
+            }
+        }
         System.out.println("\n\tPosted.");
-        System.out.println("1 to return User Panel - 2 to share another post.");
-
-        int choose = scanner.nextInt();
-        scanner.nextLine();//dummy
         }
 
 
-    public void Comment(UserClass user)
-    {
-        ArrayList<String> list = new ArrayList<>();
-        list = getComments();
+    public void Comment(UserClass user) {
+        ArrayList<String> list = new ArrayList<String>();
+        list = this.getComments();
         System.out.println("Write your comment here: ");
         String comm = scanner.nextLine();
-        list.add(user.getName()+" : "+comm);
-        setComments(list);
-        int a = getCommentCount()+1;
+
+        list.add(user.getName() + " : " + comm);
+        this.setComments(list);
+
+        //it adds 1 to comment count
+        int a = getCommentCount() + 1;
         setCommentCount(a);
     }
-
 }
