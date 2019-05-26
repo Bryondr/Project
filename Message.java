e8b5009e920cfb61aaa7f0123aed6e8924850ee4
Bryondr <44525204+Bryondr@users.noreply.github.com>
2019-05-26 16:27:21 +0300

    Update Message.java

--- Message.java
+++ Message.java
@@ -6,14 +6,8 @@ public class Message
 {
     private String message;
     private String content;
-
-    public String getMessage() { return message; }
-    public void setMessage(String message) {this.message = message; }
-
-    public String getContent() { return content; }
-    public void setContent(String content) { this.content = content; }
-
-
+    private String getterPerson;
+    private String senderPerson;
 
     //Constructor
     public Message()
@@ -21,13 +15,30 @@ public class Message
 
     }
 
+    //Getters and Setters
+    public String getMessage() { return message; }
+    public void setMessage(String message) {this.message = message; }
+
+    public String getContent() { return content; }
+    public void setContent(String content) { this.content = content; }
+
+    public String getGetterPerson() { return getterPerson; }
+    public void setGetterPerson(String getterPerson) { this.getterPerson = getterPerson; }
+
+    public String getSenderPerson() { return senderPerson; }
+    public void setSenderPerson(String senderPerson) { this.senderPerson = senderPerson; }
+
+
+
+    //Sends message to needed boxes
     public void SendMessage(UserClass user1,UserClass user2,String message,String content)
     {
-        ArrayList<Message> list = new ArrayList<>();
-        FollowSystem System = new FollowSystem();
 
-        if(System.IsEachOtherFollowing(user1, user2))
+        FollowSystem Following = new FollowSystem();
+
+        if(Following.IsEachOtherFollowing(user1, user2))
         {
+            ArrayList<Message> list = new ArrayList<Message>();
 
             list = user1.getOutgoing();
             list.add(this);
@@ -38,21 +49,30 @@ public class Message
             user2.setIncoming(list);
 
             user2.SendToNotificationBox("You got a new message from "+user1.getName()+"\nMessage is: "+getMessage()+"\nContent is: "+getContent());
+            System.out.println("Your message sent!");
 
         }
+
+        else
+            {
+                System.out.println("He should follow you for sending message");
+            }
     }
+    
+    //Messaging Panel for user
     public void MessagingPanel (UserClass user)
     {
         Scanner scanner = new Scanner(System.in);
-
+        int no = user.getFollowedList().size();
         System.out.println("Please select who do you want to send message");
-        for(int i=0; i< user.getFollowedList().size(); i++)
+        for(int i=1; i< no+1; i++)
         {
-            System.out.println(i + ". "+user.getFollowedList().get(i).getName());
+            System.out.println(i + ". "+user.getFollowedList().get(i-1).getName());
         }
-        int choose  = scanner.nextInt();
+        int choose  = scanner.nextInt()-1;
         scanner.nextLine(); // Dummy
-
+        setSenderPerson(user.getName());
+        setGetterPerson(user.getFollowedList().get(choose).getName());
         System.out.println("Please write your message: ");
         String msg = scanner.nextLine();
         setMessage(msg);
@@ -62,7 +82,5 @@ public class Message
         setContent(cont);
 
         SendMessage(user,user.getFollowedList().get(choose),getMessage(),getContent());
-        System.out.println("Your message sent!");
     }
-
 }
