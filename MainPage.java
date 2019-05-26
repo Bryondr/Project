8c3a1b295e2a61a87d2ba1cd6f6de7cb6d3954fc
Bryondr <44525204+Bryondr@users.noreply.github.com>
2019-05-26 16:25:06 +0300

    Done

--- MainPage.java
+++ MainPage.java
@@ -1,4 +1,25 @@
-package project;
+public class MainPage {
 
-public class MainPage extends Post {    
+    //Gets all posts from mainpage
+    public void getMainPage( UserClass user ){
+
+        for(int i=0; i<user.getPostList().size(); i++)
+        {
+            System.out.println("__________________");
+            System.out.println( i+1 + ". Post\n" + user.getPostList().get(i).getSharer()+"\n");
+            System.out.println(user.getPostList().get(i).getContent());
+            System.out.println(user.getPostList().get(i).getLikes() + " Likes ");
+            System.out.println(user.getPostList().get(i).getComments().size()+" Comments");
+            System.out.println("__________________");
+        }
+    }
+    //Gets post from mainpage
+    public void getMainPage( UserClass user, int i )
+    {
+        System.out.println("__________________");
+        System.out.println( i + ". Post\n" + user.getPostList().get(i-1).getSharer()+"\n");
+        System.out.println(user.getPostList().get(i-1).getContent());
+        System.out.println(user.getPostList().get(i-1).getLikes() + "Likes ");
+        System.out.println("__________________");
+    }
 }
