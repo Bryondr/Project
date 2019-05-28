9049c7387672b46cf599c862dee1c942cb42ee09
lordpac <50585545+lordpac@users.noreply.github.com>
2019-05-28 23:06:33 +0300

    Done - profil eklendi

--- Timeline.java
+++ Timeline.java
@@ -3,9 +3,10 @@ public class TimeLine
 
     //Gets all posts from user'stimeline
     public void getTimeLine( UserClass user ){
-
-        for(int i=0; i<user.getTimeLine().size(); i++)
-        {
+        System.out.println("__________________");
+        System.out.println(user.getName() + "\n" + user.getEmail() + " || " + user.getDayOfBirth()+ "." + user.getMonthOfBirth() + "." + user.getYearOfBirth() + "\n" + user.getPlace() );
+        
+        for(int i=0; i<user.getTimeLine().size(); i++){
             System.out.println("__________________");
             System.out.println( i+1 + ". Post\n" + user.getTimeLine().get(i).getSharer()+"\n");
             System.out.println(user.getTimeLine().get(i).getContent());
@@ -15,12 +16,11 @@ public class TimeLine
         }
     }
     //Gets post from user's timeline which user's select
-    public void getTimeLine( UserClass user, int i )
-    {
+    public void getTimeLine( UserClass user, int i ){
         System.out.println("__________________");
         System.out.println( i + ". Post\n" + user.getTimeLine().get(i-1).getSharer()+"\n");
         System.out.println(user.getTimeLine().get(i-1).getContent());
-        System.out.println(user.getTimeLine().get(i-1).getLikes() + "Likes ");
+        System.out.println(user.getTimeLine().get(i-1).getLikes() + " Likes ");
         System.out.println(user.getTimeLine().get(i-1).getComments().size()+" Comments");
         System.out.println("__________________");
     }
